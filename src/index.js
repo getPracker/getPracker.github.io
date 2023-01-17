abc08ba45775d556643f04fa5ace05fb04468b7b
getPracker <getpracker@gmail.com>
2023-01-17 09:42:38 +0530

    index.html shifted

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import App from './app/App';
 import store from './app/store';
 import {BrowserRouter} from "react-router-dom";
-import '../public/index.css';
+import '../index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
