a054b2a9e52f2fa14bb349c8a0cd80e8f6d060a0
getPracker <getpracker@gmail.com>
2023-01-17 18:06:18 +0530

    CSS Changes

--- src/features/Contact/Contact.js
+++ src/features/Contact/Contact.js
@@ -8,15 +8,15 @@ function Contact(){
         <div className='title'><h1>Contact ME</h1></div>
         <form action='https://formspree.io/f/xzbqzeka' method="post">
             <div className='name inline form-item'>
-                <div className='item'>Name:</div>
+                <div className='item label'>Name:</div>
                 <div className='item'><input name="name" type="text" required></input></div>
             </div>
             <div className='email inline form-item'>
-                <div className='item'>Email:</div>
+                <div className='item label'>Email:</div>
                 <div className='item'><input name="email" type="email" required></input></div>
             </div>
             <div className='name inline form-item'>
-                <div className='item'>Message:</div>
+                <div className='item label'>Message:</div>
                 <div className='item' ><textarea name="message" type="text" required></textarea></div>
             </div>
             <button value="submit">Submit</button>
