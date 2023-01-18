feb3a66889727ac355d80d615f073b91e697f4e8
getPracker <getpracker@gmail.com>
2023-01-18 13:26:25 +0530

    UI Changes - Navbar, Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ function Footer(){
           <div className="inline-grid-col text-item">@getPracker</div>
         </div>
         <ul className="inline-grid-col">
-            <li className="inline-grid-col"><a className="inline-grid-col"href="https://www.instagram.com/getPracker/" target="_blank" rel="noopener noreferrer"><img src={instagram} alt="Instagram"></img></a></li>
+            <li> <a href="https://www.instagram.com/getPracker/" target="_blank" rel="noopener noreferrer"><img src={instagram} alt="Instagram"></img></a></li>
             <li className="inline-grid-col"><a className="inline-grid-col" href="https://www.linkedin.com/in/getPracker/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="LinkedIn"></img></a></li>
             <li className="inline-grid-col"><a className="inline-grid-col" href="https://github.com/getpracker" target="_blank" rel="noopener noreferrer"><img src={github} alt="Github"></img></a></li>
             <li className="inline-grid-col"><a className="inline-grid-col" href="https://www.youtube.com/@getPracker" target="_blank" rel="noopener noreferrer"><img src={youtube} alt="Youtube"></img></a></li>
